3903a4902b45ae054202c9d4f9e0f565b060abbe
hxzeng <740473742@qq.com>
2013-12-18 19:29:06 +0800

    resource

---
+++ QRcodeInformation/src/com/qrcode/resource/Resource.java
@@ -0,0 +1,25 @@
+package com.liu.resource;
+
+/*
+ *此类存放引用程序的资源
+ */
+public class Resource {
+	public static final String HEADIMAGE = "headimage";
+	
+	
+	public static final int VERSION =3;
+	public static final String DBNAME = "qrcode.db";
+	public static final String IMAGE = "image";
+	public static final String TEXT = "text";
+	public static final String NAME = "name";
+	public static final String PHONE = "phone";
+	public static final String QQ = "QQ";
+	public static final String WEIXIN = "weixin";
+	public static final String MICROBLOG = "microblog";
+	public static final String MAILLBOX = "maillbox";
+	public static final String JOB = "job";
+	public static final String ADDRESSLISTGROUP = "addresslistgroup";
+	public static final String SEX = "sex";
+	public static final String ADDRESS = "address";
+	public static final String REMARKS = "remarks";
+}
